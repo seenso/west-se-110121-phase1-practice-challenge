a1a82a010ce3e24a38aed6d98b711238984736fb
seenso <>
2021-11-10 08:54:10 -0900

    Got characters to show up on the character-bar

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 // Your code here
+const url = "http://localhost:3000";
+
+//GET characters
+const getCharacters = () => {
+  return fetch(url+"/characters")
+  .then(response => response.json())
+  .then(characters => {
+    //loop through characters array and create <span></span> for each character.
+    characters.forEach(character => {
+      let name = character.name;
+      //Add each character to charBar.
+      document.querySelector("div#character-bar").innerHTML += `<span>${name}</span>`;
+    });
+
+    console.log("CHARBAR", document.querySelector("div#character-bar").innerHTML);
+  });
+};
+
+const handleVote = () => {
+  return fetch(url, {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+  });
+};
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  event.preventDefault();
+  getCharacters();
+});
